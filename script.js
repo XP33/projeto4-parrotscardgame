e3b00ca810e0d550693e6064f2dee7d044f4e579
Pimenta <mateuspimartins@hotmail.com>
2022-02-07 16:08:43 -0300

    final version

--- script.js
+++ script.js
@@ -1,90 +1,199 @@
-let DishType =  null;
-let DishValue = null;
-let DrinkType = null;
-let DrinkValue = null;
-let DessertType = null;
-let DessertValue = null;
-let Total = null;
+// Card Functions
+function addCards() {
+    const cardsContainer = document.querySelector(".cards-container")
+  
+    cardsContainer.innerHTML = RandomizeCards()
+  }
+  
+  function RandomizeCards() {
 
-function selectDish(DishX, DishTypeX,  DishValueX){
-
-    const selecionado= document.querySelector(".dishes .selecionado");
-    
-    if(selecionado != null) {
-        selecionado.classList.remove("selecionado");
+    const arrayOfNumbers = []
+  
+    let bgNumber = null
+    let cardStructure = ''
+  
+    for(let index = 0; index < validNumberOfCards; index++) {
+      bgNumber = getRandomNumber(validNumberOfCards / 2)
+  
+      let maxRandomBgArray = (arrayOfNumbers.filter((number) => number === bgNumber))
+  
+      // Verifies if the same background has already been aplied two times
+      while(maxRandomBgArray.length === 2) {
+        bgNumber = getRandomNumber(validNumberOfCards / 2)
+  
+        maxRandomBgArray = (arrayOfNumbers.filter((number) => number === bgNumber))
+      }
+  
+      cardStructure += `
+        <div class="card" data-identifier="card">
+        <div class="front-face face" data-identifier="front-face"></div>
+        <div class="back-face face bg${bgNumber}" data-identifier="back-face"></div>
+        </div>
+        `
+  
+      arrayOfNumbers.push(bgNumber)
     }
-    const adiciona = document.querySelector("."+DishX);
-    adiciona.classList.add("selecionado");
- 
-    DishType = DishTypeX;
-    DishValue = DishValueX;
-
-    VerifyButton();
-}
-function selectDrink(DrinkX, DrinkTypex, DrinkValueX){
-
-    const selecionado = document.querySelector(".drinks .selecionado");
-    if(selecionado != null){
-        selecionado.classList.remove("selecionado");
+    return cardStructure
+  }
+  
+  function allCardsUp() {
+    if(youWinComparator === validNumberOfCards) {
+      timer.stopTimer()
+  
+      setTimeout(() => {
+        alert(`Você ganhou em ${timer.element.innerHTML} segundos e ${turnedCardsCount} jogadas!`)
+      }, 500)
+  
+      setTimeout(() => {
+        const answer = prompt('Deseja testar sua memória outra vez? Y/N')
+        const validAnswer = answer.trim().toUpperCase()
+  
+        if(validAnswer === 'Y' || validAnswer === 'YES') {
+          restart()
+        }
+      }, 500)
     }
-
-    const adiciona = document.querySelector("."+DrinkX);
-    adiciona.classList.add("selecionado");
-
-    DrinkType = DrinkTypex;
-    DrinkValue = DrinkValueX;
-
-    VerifyButton();
-}
-function selectDessert(DessertX, DessertTypeX, DessertValueX ){
-
-    const selecionado = document.querySelector(".desserts .selecionado");
-    if(selecionado != null){
-        selecionado.classList.remove("selecionado");
+  }
+  
+  // Card click functions
+  function CardClick() {
+    const cards = document.querySelectorAll('.card')
+  
+    cards.forEach((card) => {
+      card.addEventListener('click', () => {
+        clickedCards.push(card)
+        turnedCardsCount++
+        clicks++
+  
+        // Start counting time
+        if(turnedCardsCount === 1) {
+          timer.startTimer()
+        }
+  
+        const firstClick = (clicks === 1)
+        const secondClick = (clicks === 2)
+        const offClick = (clicks >= 3)
+  
+        if(firstClick) {
+          flipCards(card)
+        } else if(secondClick) {
+          flipCards(card)
+          compareClickedCards()
+        } else if(offClick) {
+          turnedCardsCount--
+          clicks--
+        }
+      })
+    })
+  }
+  
+  function compareClickedCards() {
+    const firstClickedCardClassList = (clickedCards[0].children[1].classList.value)
+    const secondClickedCardClassList = (clickedCards[1].children[1].classList.value)
+  
+    const cardsAreEqual = firstClickedCardClassList == secondClickedCardClassList
+  
+    if(cardsAreEqual) {
+      youWinComparator += 2
+  
+      resetClickCount()
+      allCardsUp()
+  
+    } else {
+      flipCardsBack()
     }
-
-
-    const adiciona = document.querySelector("."+DessertX);
-    adiciona.classList.add("selecionado");
-
-    DessertType = DessertTypeX;
-    DessertValue = DessertValueX;
-
-    VerifyButton();
-}
-function VerifyButton(){
-
-    if((DishType != null) && (DrinkType != null) && (DessertType != null)){
-        
-        const grey = document.querySelector(".grey");
-        grey.classList.add("escondido");
-
-        const green = document.querySelector(".green");
-        green.classList.remove("escondido");
+  }
+  
+  function resetClickCount() {
+    clicks = 0
+  
+    for(let index = 0; index <= clickedCards.length; index++) {
+      clickedCards.pop()
     }
-}
-function soma(a,b,c ){
-    let Dish = parseFloat(a);
-    let Drink = parseFloat(b);
-    let comida = parseFloat(c);
-    let Total = (Dish+Drink+comida);
-
-    return Total.toFixed(2);
-}
- 
-function enviaOrdem(){
-    let nome = prompt("Qual o seu nome?");
-    let endereco  = prompt(nome + ", Qual é seu endereço? ");
-
-    const mensagem = "Olá, gostaria de fazer um pedido:\n" 
-    +"- Prato: " + DishType 
-    + "\n- Bebida: " + DrinkType 
-    + "\n- Sobremesa: "+ DessertType 
-    + "\nTotal:  R$ " + soma(DishValue, DrinkValue, DessertValue).toString().replace('.',',')
-    + "\n\n Nome: " + nome 
-    + " \n Endereço: " + endereco;
-
-    let url = 'https://wa.me/5534999091797?text='+encodeURIComponent(mensagem);
-
-    window.open(url);
-}
+  }
+  
+  function flipCards(card) {
+    // Flip card
+    card.querySelector('.back-face').style.transform = 'rotateY(0deg)'
+    card.querySelector('.front-face').style.transform = 'rotateY(-180deg)'
+  }
+  
+  function flipCardsBack() {
+    // Return card to initial position
+    clickedCards.forEach((card) => {
+      setTimeout(() => {
+        card.querySelector('.back-face').style.transform = 'rotateY(180deg)'
+        card.querySelector('.front-face').style.transform = 'rotateY(0deg)'
+      }, 1000)
+    })
+    resetClickCount()
+  }
+  
+  // Get valid number of cards
+  function getNumCards() {
+    const numberOfCards = parseInt(prompt("Para começar o jogo escolha um número PAR entre 4 e 14"))
+  
+    const validateEvenNumber = (numberOfCards % 2 == 0)
+    const validateNumberOfCards = ((numberOfCards >= 4) && (numberOfCards <= 14))
+    const isNotValid = ((!validateEvenNumber === false) && (!validateNumberOfCards === false))
+  
+    if(isNotValid !== true) {
+      getNumCards();
+    } else {
+      return numberOfCards;
+    }
+  }
+  
+  // Randomizer
+  function getRandomNumber(number) {
+    const randomNumber = (Math.floor(Math.random() * number))
+  
+    return randomNumber + 1
+  }
+  
+  // Restar game -> Bonus
+  function restart() {
+    youWinComparator = 0
+    turnedCardsCount = 0
+    resetClickCount()
+  
+    timer.element.innerHTML = '0'
+  
+    const cardsContainer = document.querySelector('.cards-container')
+    cardsContainer.innerHTML = ''
+  
+    validNumberOfCards = getNumCards()
+  
+    setTimeout(() => {addCards()}, 500)
+    setTimeout(() => {CardClick()}, 500)
+  }
+  
+  // Timer Functions - Bonus (training object usage)
+  const timer = {
+    element: document.querySelector('header .timer'),
+    interval: number = 0,
+  
+    startTimer() {
+      const interval = setInterval(() => {
+        this.element.innerHTML = parseInt(this.element.innerHTML) + 1
+      }, 1000)
+  
+      this.interval = interval
+    },
+  
+    stopTimer() {
+      clearInterval(this.interval)
+    }
+  }
+  
+  // Declarations
+  
+  let validNumberOfCards = getNumCards()
+  const clickedCards = []
+  
+  let youWinComparator = 0
+  let turnedCardsCount = 0
+  let clicks = 0
+  
+  addCards()
+  CardClick()
